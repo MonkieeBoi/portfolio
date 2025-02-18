8d0650625fcc80f9cb9d038ccc7b0f173fbd6365
MonkieeBoi <danny.pham26425@gmail.com>
2025-02-18 23:08:54 +1100

    fix: wrap instead of switching to flex-row

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -4,7 +4,7 @@ import Project from "./Project";
 
 export default function Projects() {
     return (
-        <section className="bg-gray-800 p-10 relative w-screen flex flex-col md:flex-row items-center justify-center gap-10">
+        <section className="bg-gray-800 p-10 relative w-screen flex flex-row flex-wrap items-center justify-center gap-10">
             <Project
                 pTitle="spnw"
                 source="https://github.com/yunz-dev/spnw-api"
